05dcfe92174f835d3f6dc1fbaf3f5b1d8187970b
harshitksinghai <harshitksinghai.official@gmail.com>
2025-05-30 11:44:31 +0530

    initial commit h2

---
+++ src/test/java/com/harshitksinghai/shepherd_backend/ShepherdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshitksinghai.shepherd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShepherdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

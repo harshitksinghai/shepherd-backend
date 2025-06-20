c210fc8cc2fcd3f222d54cb0202bcee03bbb87da
harshitksinghai <harshitksinghai.official@gmail.com>
2025-06-20 16:46:03 +0530

    scheduler1 setup

---
+++ src/main/java/com/harshitksinghai/shepherd_backend/TaskService/SchedulerAlgorithm/Scheduler1.java
@@ -0,0 +1,19 @@
+package com.harshitksinghai.shepherd_backend.TaskService.SchedulerAlgorithm;
+
+public class Scheduler1 {
+    public static void main(String[] args) {
+
+        System.out.println("Scheduled Task Order:");
+
+    }
+
+    public static void taskDataProvider() {
+        // return data that will be used in taskScheduler method
+    }
+
+    public static void taskScheduler() {
+        // main scheduling method
+    }
+
+    // more helper methods as needed
+}
